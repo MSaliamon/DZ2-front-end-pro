9d9badfe2238e6c057bf8363b392df38ca75b1fd
MSaliamon <maksimsalamon181@gmail.com>
2023-11-03 13:21:36 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let number1 = prompt("введіть число");
+let number2 = prompt("введіть число");
+let number3 = prompt("введіть число");
+const suma = (Number (number1)+ Number (number2)+ Number (number3))/3;
+alert(`${suma}`);
